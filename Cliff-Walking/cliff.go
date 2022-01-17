e42694e396ccb10670742b9b9fd72e9d6f02d9a5
lonelymeow <131701250+lonelymeow@users.noreply.github.com>
2022-01-17 15:42:56 +0000

    Add link

--- Cliff-Walking/cliff.go
+++ Cliff-Walking/cliff.go
@@ -44,3 +44,44 @@ func (q *QLearningTD) Initialize() {
 	q.Sm = 12
 
 	Actions := 4 // up, down, left, right
+
+	q.ini_n = 0 //Start
+	q.ini_m = 0
+
+	q.ter_n = 0 //Goal
+	q.ter_m = 11
+
+	q.Qn = Actions
+	q.Qm = q.Sn * q.Sm
+
+	q.Q = make([][]float64, Actions)
+
+	for i := 0; i < Actions; i++ {
+		q.Q[i] = make([]float64, q.Sn*q.Sm)
+	}
+
+	for i := 0; i < Actions; i++ {
+		for j := 0; j < q.Sn*q.Sm; j++ {
+			q.Q[i][j] = rand.Float64()
+		}
+	}
+
+	q.SetQAll(q.ter_n, q.ter_m, 0)
+
+}
+
+func (q *QLearningTD) Pi() {
+	for i := q.Sn - 1; i >= 0; i-- {
+		for j := 0; j < q.Sm; j++ {
+			if i == q.ter_n && j == q.ter_m {
+				fmt.Print(" G")
+			} else {
+				switch q.GetAction(i, j) {
+				case Up:
+					fmt.Print(" U")
+				case Down:
+					fmt.Print(" D")
+				case Left:
+					fmt.Print(" L")
+				case Right:
+					fmt.Print(" R")
