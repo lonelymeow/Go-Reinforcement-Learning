4576f5cb2a88f941feb64e40f49c20593db6fc59
lonelymeow <131701250+lonelymeow@users.noreply.github.com>
2022-02-25 18:07:08 +0000

    Fix translations

--- Windy-Gridworld/windy.go
+++ Windy-Gridworld/windy.go
@@ -26,3 +26,51 @@ type SarsaTD struct {
 	Sm int
 
 	ter_n int
+	ter_m int
+	ini_n int
+	ini_m int
+
+	α float64
+	ε float64
+	γ float64
+}
+
+func (q *SarsaTD) Initialize() {
+
+	q.α = 0.5
+	q.ε = 0.1
+	q.γ = 1
+
+	q.Sn = 7
+	q.Sm = 10
+
+	Actions := 4 //up, down, left, right
+
+	q.ini_n = 3
+	q.ini_m = 0
+
+	q.ter_n = 3
+	q.ter_m = 7
+
+	q.Qn = Actions
+	q.Qm = q.Sn * q.Sm
+
+	q.Q = make([][]float64, Actions)
+
+	for i := 0; i < Actions; i++ {
+		q.Q[i] = make([]float64, q.Sn*q.Sm)
+	}
+
+	for i := 0; i < Actions; i++ {
+		for j := 0; j < q.Sn*q.Sm; j++ {
+			q.Q[i][j] = rand.Float64()
+		}
+	}
+
+	q.SetQAll(q.ter_n, q.ter_m, 0)
+
+	q.Storm = []int{0, 0, 0, 1, 1, 1, 2, 2, 1, 0}
+
+}
+
+func main() {
