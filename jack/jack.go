2ba6cd091f87e840159cb18b9855cfe577d219bf
lonelymeow <131701250+lonelymeow@users.noreply.github.com>
2022-03-09 23:07:31 +0000

    Update version to 0.8.0

--- jack/jack.go
+++ jack/jack.go
@@ -38,3 +38,48 @@ const (
 type State struct {
 	V float64
 	π Action
+}
+
+type Action struct {
+	action1 int
+	action2 int
+}
+
+type Mat [max_car_location_1 + 1][max_car_location_2 + 1]State
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	S := get_all_states()
+	S = policy_iteration(S)
+	print_mat(S)
+	print_cars(S)
+}
+
+func print_cars(S Mat) {
+	for i := 0; i <= max_car_location_1; i++ {
+		for j := 0; j <= max_car_location_2; j++ {
+			fmt.Print(math.Abs(float64(S[i][j].π.action1)), " ")
+		}
+		fmt.Println("")
+	}
+}
+
+func print_mat(S Mat) {
+	for i := 0; i <= max_car_location_1; i++ {
+		for j := 0; j <= max_car_location_2; j++ {
+			fmt.Print("(i: ", i, " j:", j, " : ", S[i][j])
+		}
+		fmt.Println("")
+	}
+}
+
+func update_π(S Mat) (bool, Mat) {
+	stable := true
+	for i := 0; i <= max_car_location_1; i++ {
+		for j := 0; j <= max_car_location_2; j++ {
+			_action := S[i][j].π
+
+			_i := int(math.Min(float64(i+_action.action1), float64(max_car_location_1)))
+			_j := int(math.Min(float64(j+_action.action2), float64(max_car_location_2)))
+			max := S[_i][_j].V
+			for _, a := range get_actions(i, j) {
