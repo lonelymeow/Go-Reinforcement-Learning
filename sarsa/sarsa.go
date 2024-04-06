831d31c198da0576df6b1178fe9b5c20f7e0c444
lonelymeow <131701250+lonelymeow@users.noreply.github.com>
2024-04-06 16:52:43 +0000

    add one

--- sarsa/sarsa.go
+++ sarsa/sarsa.go
@@ -64,4 +64,8 @@ func learn(state State, action string, target float64, vf *ValueFunction) {
 	delta := make([]float64, len(estimations))
 
 	for idx := 0; idx < len(delta); idx++ {
-		delta[idx] = vf.Alpha * (target 
+		delta[idx] = vf.Alpha * (target - estimations[idx])
+
+	}
+
+	for feature := ran
